fe529ee3d7075f4cc5729a5bfa64f5d31b8ff6ef
Arthur Araujo <arthur.araujo@tutanota.com>
2024-05-31 14:21:14 -0300

    Create welcome screen

--- app/src/main/java/cc/wordview/app/MainActivity.kt
+++ app/src/main/java/cc/wordview/app/MainActivity.kt
@@ -1,48 +1,84 @@
 package cc.wordview.app
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import cc.wordview.app.ui.theme.Accent
+import androidx.compose.ui.unit.sp
 
-import cc.wordview.app.ui.theme.PurpleBlack
+import cc.wordview.app.ui.theme.Typography
 import cc.wordview.app.ui.theme.WordViewTheme
+import cc.wordview.app.ui.theme.poppinsFamily
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             WordViewTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Box(
                         modifier = Modifier
                             .fillMaxSize()
-                            .background(PurpleBlack),
+                            .padding(innerPadding),
                         contentAlignment = Alignment.Center
                     ) {
-                        WVButton("Start learning", onClick = { print("hello") }, marginTop = 0.dp)
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Image(
+                                modifier = Modifier.size(120.dp),
+                                painter = painterResource(id = R.drawable.wvicon),
+                                contentDescription = "WordView icon"
+                            )
+                            Text(
+                                text = "WordView",
+                                textAlign = TextAlign.Center,
+                                style = Typography.titleLarge,
+                                fontWeight = FontWeight.Bold,
+                                fontFamily = poppinsFamily,
+                                fontSize = 42.sp
+                            )
+                            Text(
+                                text = "The smart way to learn\na language",
+                                textAlign = TextAlign.Center,
+                                style = Typography.titleLarge,
+                                fontWeight = FontWeight.SemiBold
+                            )
+                            Spacer(Modifier.size(120.dp))
+                            WVButton(
+                                "Start learning",
+                                onClick = { print("hello") })
+                            Spacer(Modifier.size(10.dp))
+                            WVButton(
+                                "I already have an account",
+                                onClick = { print("hello") },
+                            )
+                        }
                     }
                 }
             }
@@ -59,24 +95,14 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun WVButton(text: String, onClick: () -> Unit, marginTop: Dp) {
+fun WVButton(text: String, onClick: () -> Unit) {
     Button(
         onClick = { onClick() },
         modifier = Modifier
-            .width(320.dp)
-            .height(60.dp)
-            .padding(PaddingValues(top = marginTop)),
-        shape = RoundedCornerShape(20.dp),
-        colors = ButtonDefaults.buttonColors(containerColor = Accent),
+            .width(360.dp)
+            .height(60.dp),
+        shape = RoundedCornerShape(20.dp)
     ) {
         Text(text = text)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WordViewTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/cc/wordview/app/ui/theme/TextSnippets.kt
@@ -0,0 +1,15 @@
+package cc.wordview.app.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import cc.wordview.app.R
+
+val poppinsFamily = FontFamily(
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_bold, FontWeight.Bold)
+)
