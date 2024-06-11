672deac5a9acf2e8101cbea061b33c2c9dfa517e
Arthur Araujo <arthur.araujo@tutanota.com>
2024-06-11 11:48:17 -0300

    Add player screen placeholders

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("io.coil-kt:coil:2.6.0")
     implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("androidx.compose.material:material-icons-extended:1.6.7")
     testImplementation(libs.junit)
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/cc/wordview/app/ui/screens/Player.kt
+++ app/src/main/java/cc/wordview/app/ui/screens/Player.kt
@@ -18,25 +18,39 @@
 package cc.wordview.app.ui.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.SkipNext
+import androidx.compose.material.icons.filled.SkipPrevious
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import cc.wordview.app.currentSong
 import cc.wordview.app.extensions.goBack
+import cc.wordview.app.ui.components.WVIconButton
+import cc.wordview.app.ui.theme.DefaultRoundedCornerShape
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -64,8 +78,59 @@ fun Player(navController: NavHostController) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding),
-            contentAlignment = Alignment.Center
+            contentAlignment = Alignment.TopCenter
         ) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp)
+                    .height(500.dp),
+                color = MaterialTheme.colorScheme.primaryContainer,
+                shape = DefaultRoundedCornerShape
+            ) {
+                Column(modifier = Modifier.fillMaxSize().padding(18.dp)) {
+                    /*TODO*/
+                }
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                WVIconButton(
+                    onClick = { /*TODO*/ },
+                    imageVector = Icons.Filled.SkipPrevious,
+                    size = 75.dp,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.background,
+                        contentColor = MaterialTheme.colorScheme.primary
+                    ),
+                )
+                Spacer(modifier = Modifier.size(10.dp))
+                WVIconButton(
+                    onClick = { /*TODO*/ },
+                    imageVector = Icons.Filled.PlayArrow,
+                    size = 80.dp,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.surface,
+                        contentColor = MaterialTheme.colorScheme.primary
+                    ),
+                )
+                Spacer(modifier = Modifier.size(10.dp))
+                WVIconButton(
+                    onClick = { /*TODO*/ },
+                    imageVector = Icons.Filled.SkipNext,
+                    size = 75.dp,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.background,
+                        contentColor = MaterialTheme.colorScheme.primary
+                    ),
+                )
+            }
         }
     }
 }
