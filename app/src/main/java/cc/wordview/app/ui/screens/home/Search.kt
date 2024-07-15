83fb236728480926cfdb604dd1053745de5e683d
Arthur Araujo <arthur.araujo@tutanota.com>
2024-07-15 19:46:00 -0300

    Display search results and make them redirect to the player on click

--- app/src/main/java/cc/wordview/app/ui/screens/home/Search.kt
+++ app/src/main/java/cc/wordview/app/ui/screens/home/Search.kt
@@ -17,17 +17,27 @@
 
 package cc.wordview.app.ui.screens.home
 
+import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -35,6 +45,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -43,13 +54,26 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import cc.wordview.app.R
 import cc.wordview.app.api.APICallback
+import cc.wordview.app.api.Video
+import cc.wordview.app.api.VideoSearchResult
 import cc.wordview.app.api.search
+import cc.wordview.app.currentSong
 import cc.wordview.app.extensions.goBack
+import cc.wordview.app.ui.screens.util.Screen
+import cc.wordview.app.ui.theme.Typography
+import coil.compose.AsyncImage
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
+@SuppressLint("MutableCollectionMutableState")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Search(navController: NavHostController) {
@@ -59,11 +83,20 @@ fun Search(navController: NavHostController) {
     var isSearching by remember { mutableStateOf(false) }
     var leadingIcon by remember { mutableStateOf(Icons.Filled.Search) }
     var waitingForResponse by remember { mutableStateOf(false) }
+    var results by remember { mutableStateOf(ArrayList<VideoSearchResult>()) }
 
     val callback = object : APICallback {
         override fun onSuccessResponse(response: String?) {
             if (response != null) {
                 Log.i("Search", "$response")
+
+
+                val typeToken = object : TypeToken<List<VideoSearchResult>>() {}.type
+                results = Gson().fromJson<List<VideoSearchResult>>(
+                    response,
+                    typeToken
+                ) as ArrayList<VideoSearchResult>
+
                 waitingForResponse = false
             }
         }
@@ -71,6 +104,19 @@ fun Search(navController: NavHostController) {
         override fun onErrorResponse(response: String?) {
             TODO("Not yet implemented")
         }
+    }
+
+
+    fun playResult(result: VideoSearchResult) {
+        val video = Video(
+            result.id,
+            result.title,
+            result.channel,
+            "https://img.youtube.com/vi/${result.id}/0.jpg"
+        )
+
+        currentSong = video
+        navController.navigate(Screen.Player.route)
     }
 
     Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
@@ -110,18 +156,72 @@ fun Search(navController: NavHostController) {
             }
         }
     }) { innerPadding ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            contentAlignment = Alignment.Center
-        ) {
-            if (waitingForResponse) {
+        if (waitingForResponse) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                contentAlignment = Alignment.Center
+            ) {
                 CircularProgressIndicator(
                     modifier = Modifier.width(64.dp),
                     color = MaterialTheme.colorScheme.secondary,
                     trackColor = MaterialTheme.colorScheme.surfaceVariant,
                 )
+            }
+        } else {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                for (result in results) {
+                    Spacer(Modifier.size(12.dp))
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(72.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.background,
+                        ),
+                        onClick = { playResult(result) }
+                    ) {
+                        Row {
+                            Surface(
+                                modifier = Modifier
+                                    .size(72.dp)
+                                    .padding(8.dp),
+                                shape = RoundedCornerShape(5.dp)
+                            ) {
+                                AsyncImage(
+                                    model = "https://img.youtube.com/vi/${result.id}/0.jpg",
+                                    placeholder = painterResource(id = R.drawable.radio),
+                                    error = painterResource(id = R.drawable.radio),
+                                    contentDescription = "${result.title} cover",
+                                    modifier = Modifier.fillMaxSize(),
+                                    contentScale = ContentScale.Crop,
+                                )
+                            }
+                            Column(Modifier.padding(8.dp)) {
+                                Text(
+                                    text = result.title,
+                                    style = Typography.labelMedium,
+                                    textAlign = TextAlign.Left,
+                                    modifier = Modifier.fillMaxWidth(),
+                                )
+                                Text(
+                                    text = result.channel,
+                                    style = Typography.labelSmall,
+                                    textAlign = TextAlign.Left,
+                                    modifier = Modifier.fillMaxWidth(),
+                                    color = MaterialTheme.colorScheme.inverseSurface
+                                )
+                            }
+                        }
+                    }
+                }
             }
         }
     }
