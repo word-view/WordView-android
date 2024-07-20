2dd7e5ddcf49cb0b3cddb36420dafc5b072de506
Arthur Araujo <arthur.araujo@tutanota.com>
2024-07-20 07:37:17 -0300

    Lock app screen orientation to portrait

--- app/src/main/java/cc/wordview/app/MainActivity.kt
+++ app/src/main/java/cc/wordview/app/MainActivity.kt
@@ -17,23 +17,23 @@
 
 package cc.wordview.app
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import cc.wordview.app.api.Video
 import cc.wordview.app.ui.screens.util.Screen
 import cc.wordview.app.ui.theme.WordViewTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
+        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
--- app/src/main/java/cc/wordview/app/ui/screens/home/Home.kt
+++ app/src/main/java/cc/wordview/app/ui/screens/home/Home.kt
@@ -137,5 +137,4 @@ fun Home(navHostController: NavHostController) {
             }
         }
     }
-
 }
