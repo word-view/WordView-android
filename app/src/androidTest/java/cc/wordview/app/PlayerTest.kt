8e77113cbe782759d2ed19585aadb60de6d237a2
Arthur Araujo <arthur.araujo@tutanota.com>
2024-07-21 18:24:12 -0300

    Add a basic test for the Player screen

---
+++ app/src/androidTest/java/cc/wordview/app/PlayerTest.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024 Arthur Araujo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package cc.wordview.app
+
+import androidx.compose.ui.test.click
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTouchInput
+import androidx.navigation.compose.rememberNavController
+import cc.wordview.app.api.Video
+import cc.wordview.app.ui.screens.home.Player
+import cc.wordview.app.ui.theme.WordViewTheme
+import cc.wordview.app.util.AudioPlayer
+import org.junit.Rule
+import org.junit.Test
+
+class PlayerTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun renders() {
+        SongViewModel.setVideo(Video(
+            "",
+            "No Title",
+            "REOL",
+            ""
+        ))
+
+        composeTestRule.setContent {
+            WordViewTheme {
+                Player(navController = rememberNavController())
+            }
+        }
+
+        composeTestRule.onNodeWithText("No Title").assertExists()
+    }
+}
