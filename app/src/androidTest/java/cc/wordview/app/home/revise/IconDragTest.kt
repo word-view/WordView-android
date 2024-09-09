631ed9efc45b3d32339af2cfd25fe6a9f9746c5e
Arthur Araujo <arthur.araujo@tutanota.com>
2024-09-09 05:06:35 -0300

    Add `IconDrag` visual test

---
+++ app/src/androidTest/java/cc/wordview/app/home/revise/IconDragTest.kt
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2024 Arthur Araujo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package cc.wordview.app.home.revise
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performTouchInput
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import cc.wordview.app.subtitle.initializeIcons
+import cc.wordview.app.ui.screens.home.ReviseResults
+import cc.wordview.app.ui.screens.home.model.WordReviseViewModel
+import cc.wordview.app.ui.screens.home.revise.IconDrag
+import cc.wordview.app.ui.screens.home.revise.ReviseScreen
+import cc.wordview.app.ui.screens.util.Screen
+import cc.wordview.app.ui.theme.WordViewTheme
+import cc.wordview.gengolex.languages.Word
+import me.zhanghai.compose.preference.ProvidePreferenceLocals
+import org.junit.Rule
+import org.junit.Test
+
+class IconDragTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private val viewModel = WordReviseViewModel
+
+    private fun setupScreen(autoAdvance: Boolean = true, appendWords: Boolean = false) {
+        composeTestRule.mainClock.autoAdvance = autoAdvance
+
+        if (appendWords) {
+            viewModel.appendWord(Word("tear", "lágrima"))
+            viewModel.appendWord(Word("rain", "chuva"))
+            viewModel.appendWord(Word("voice", "voz"))
+
+            viewModel.setWord(Word("tear", "lágrima"))
+        }
+
+        composeTestRule.setContent {
+            val navController = rememberNavController()
+
+            initializeIcons()
+
+            WordViewTheme {
+                ProvidePreferenceLocals {
+                    NavHost(navController = navController, startDestination = "icon-drag") {
+                        composable("icon-drag") {
+                            IconDrag(navController)
+                        }
+
+                        composable(Screen.ReviseResults.route) {
+                            ReviseResults(navController)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun renders() {
+        setupScreen(appendWords = true)
+
+        composeTestRule.onNodeWithTag("root").assertExists()
+        composeTestRule.onNodeWithTag("drag").assertExists()
+        composeTestRule.onNodeWithTag("top-word").assertExists()
+        composeTestRule.onNodeWithTag("down-word").assertExists()
+
+    }
+
+    @Test
+    fun dragUp() {
+        setupScreen(appendWords = true)
+
+        composeTestRule.onNodeWithTag("drag").performTouchInput {
+            down(center)
+            moveBy(Offset(0f, 500f))
+            up()
+        }
+        composeTestRule.waitUntil(10_000) { viewModel.screen.value == ReviseScreen.Presenter.route }
+    }
+
+    @Test
+    fun dragDown() {
+        setupScreen(appendWords = true)
+
+        composeTestRule.onNodeWithTag("drag").performTouchInput {
+            down(center)
+            moveBy(Offset(0f, -500f))
+            up()
+        }
+        composeTestRule.waitUntil(10_000) { viewModel.screen.value == ReviseScreen.Presenter.route }
+    }
+}
--- app/src/main/java/cc/wordview/app/ui/screens/home/revise/IconDrag.kt
+++ app/src/main/java/cc/wordview/app/ui/screens/home/revise/IconDrag.kt
@@ -17,7 +17,6 @@
 
 package cc.wordview.app.ui.screens.home.revise
 
-import android.util.Log
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
@@ -37,6 +36,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -122,12 +122,15 @@ fun IconDrag(
     }
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .testTag("root"),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
         topWord?.let {
             Text(
+                modifier = Modifier.testTag("top-word"),
                 text = it.word,
                 textAlign = TextAlign.Center,
                 style = Typography.displayMedium,
@@ -154,10 +157,13 @@ fun IconDrag(
                         offsetX += dragAmount.x
                         offsetY += dragAmount.y
                     })
+                .testTag("drag")
         ) {
             getIconForWord(currentWord.parent)?.let {
                 Image(
-                    modifier = Modifier.size(130.dp),
+                    modifier = Modifier
+                        .size(130.dp)
+                        .testTag("icon"),
                     painter = it,
                     contentDescription = currentWord.word
                 )
@@ -165,6 +171,7 @@ fun IconDrag(
         }
         downWord?.let {
             Text(
+                modifier = Modifier.testTag("down-word"),
                 text = it.word,
                 textAlign = TextAlign.Center,
                 style = Typography.displayMedium,
