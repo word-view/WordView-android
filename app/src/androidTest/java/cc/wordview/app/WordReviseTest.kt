4ded635a053c158824bbce59614156c3dd717482
Arthur Araujo <arthur.araujo@tutanota.com>
2024-09-17 23:13:54 -0300

    Fix broken `WordReviseTest`

--- app/src/androidTest/java/cc/wordview/app/WordReviseTest.kt
+++ app/src/androidTest/java/cc/wordview/app/WordReviseTest.kt
@@ -17,13 +17,17 @@
 
 package cc.wordview.app
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import cc.wordview.app.ui.screens.home.WordRevise
 import cc.wordview.app.ui.screens.home.model.WordReviseViewModel
 import cc.wordview.app.ui.screens.home.revise.algo.ReviseWord
+import cc.wordview.app.ui.screens.util.Screen
 import cc.wordview.app.ui.theme.WordViewTheme
 import cc.wordview.gengolex.languages.Word
 import org.junit.Rule
@@ -43,7 +47,16 @@ class WordReviseTest {
         viewModel.appendWord(ReviseWord(Word("5", "5")))
 
         composeTestRule.setContent {
-            WordViewTheme { WordRevise(rememberNavController()) }
+            WordViewTheme {
+                val navController = rememberNavController()
+                NavHost(navController , startDestination = "word-revise") {
+                    composable(Screen.WordRevise.route) {
+                        WordRevise(navController)
+                    }
+
+                    composable("home") { Box {} }
+                }
+            }
         }
     }
 
