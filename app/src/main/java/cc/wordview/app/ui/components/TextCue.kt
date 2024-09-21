5bc45679a7103422c5a2a5a78f00051b8ebe1bb5
Arthur Araujo <arthur.araujo@tutanota.com>
2024-09-20 22:28:44 -0300

    Add tests for `TextCue`

--- app/src/main/java/cc/wordview/app/ui/components/TextCue.kt
+++ app/src/main/java/cc/wordview/app/ui/components/TextCue.kt
@@ -24,8 +24,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Icon
@@ -34,8 +32,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Constraints
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cc.wordview.app.subtitle.WordViewCue
 import cc.wordview.app.subtitle.getIconForWord
