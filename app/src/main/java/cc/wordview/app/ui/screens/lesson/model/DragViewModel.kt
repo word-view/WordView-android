d5a905ea19b340a8c6b06edf3aaf5e61bd318002
Arthur Araujo <arthur.araujo@tutanota.com>
2025-01-14 05:39:43 -0300

    Update year to 2025

--- app/src/main/java/cc/wordview/app/ui/screens/lesson/model/DragViewModel.kt
+++ app/src/main/java/cc/wordview/app/ui/screens/lesson/model/DragViewModel.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Arthur Araujo
+ * Copyright (c) 2025 Arthur Araujo
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
