d93f36691a1eb8b3bd3fe6e13b0e0b436ffd1bff
Arthur Araujo <arthur.araujo@tutanota.com>
2025-08-11 21:55:06 -0300

    Make all topbars that have a back button use `BackTopAppBar` composable

--- app/src/main/java/cc/wordview/app/ui/activities/home/composables/Settings.kt
+++ app/src/main/java/cc/wordview/app/ui/activities/home/composables/Settings.kt
@@ -22,19 +22,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.outlined.Carpenter
 import androidx.compose.material.icons.outlined.NetworkPing
 import androidx.compose.material.icons.outlined.Translate
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -43,10 +37,10 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.navigation.NavHostController
 import cc.wordview.app.BuildConfig
 import cc.wordview.app.R
-import cc.wordview.app.extensions.displayName
 import cc.wordview.app.extensions.goBack
 import cc.wordview.app.extensions.localizedDisplayName
 import cc.wordview.app.misc.AppSettings
+import cc.wordview.app.ui.components.BackTopAppBar
 import cc.wordview.app.ui.theme.poppinsFamily
 import cc.wordview.gengolex.Language
 import me.zhanghai.compose.preference.ListPreferenceType
@@ -59,25 +53,14 @@ fun Settings(navController: NavHostController) {
     val context = LocalContext.current
 
     Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
-        TopAppBar(
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.background,
-                titleContentColor = LocalContentColor.current
-            ),
+        BackTopAppBar(
             title = {
                 Text(
                     stringResource(R.string.settings),
                     fontFamily = poppinsFamily,
                 )
             },
-            navigationIcon = {
-                IconButton(onClick = { navController.goBack() }) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = "Go back"
-                    )
-                }
-            }
+            onClickBack = { navController.goBack() }
         )
     }) { innerPadding ->
         Box(
